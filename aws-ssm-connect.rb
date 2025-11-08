7f8b77d04c5d1a7c0f27440b2d4ebb95f10f115e
Nicodemus Karisa <nkarisa@ke.ci.org>
2025-11-08 15:33:57 +0300

    Added a brew formula update

--- aws-ssm-connect.rb
+++ aws-ssm-connect.rb
@@ -4,7 +4,7 @@ class AwsSsmConnect < Formula
   version "v1.0.0"
   
   # Specify the URL for the source archive (usually a tarball of the release)
-  url "https://github.com/nkarisa/aws-ssm-connect/v1.0.0.tar.gz"
+  url "https://github.com/nkarisa/homebrew-aws-ssm-connect/v1.0.0.tar.gz"
   # Replace with the actual SHA-256 hash of your v1.0.0 tarball
   sha256 "7c151c74a016b0e41308f61425571563f7447778"
 
