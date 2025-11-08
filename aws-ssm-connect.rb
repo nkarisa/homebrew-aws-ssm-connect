41f83f59c29d793f31545516c08aa3e49773a438
Nicodemus Karisa <nkarisa@ke.ci.org>
2025-11-08 15:37:40 +0300

    Added a brew formula update

--- aws-ssm-connect.rb
+++ aws-ssm-connect.rb
@@ -2,11 +2,11 @@ class AwsSsmConnect < Formula
   desc "Interactive CLI tool for SSM access via AWS SSO"
   homepage "https://github.com/nkarisa/homebrew-aws-ssm-connect"
   version "v1.0.0"
-  
+
   # Specify the URL for the source archive (usually a tarball of the release)
   url "https://github.com/nkarisa/homebrew-aws-ssm-connect/archive/v1.0.0.tar.gz"
   # Replace with the actual SHA-256 hash of your v1.0.0 tarball
-  sha256 "7c151c74a016b0e41308f61425571563f7447778"
+  sha256 "7323077a159cdffd59e6fa1abf56da7ce06407b270105fab412ba0e8c388450d"
 
   # Go is used to build the source code
   depends_on "go" => :build
